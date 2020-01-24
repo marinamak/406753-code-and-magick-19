ae2cf4351786c3c1e8961ea4d485310f9dfb13e4
Marina <marisha.april@gmail.com>
2020-01-24 17:35:06 +0400

    ДЗ2: Исправила ошибки

--- js/stat.js
+++ js/stat.js
@@ -10,7 +10,7 @@ var BAR_GAP = 50;
 var BAR_HEIGHT = 150;
 var BAR_WIDTH = 40;
 
-var getMaxElement = function(arr) {
+var getMaxElement = function (arr) {
   if (arr.length > 0) {
     var maxElement = arr[0];
 
@@ -19,11 +19,11 @@ var getMaxElement = function(arr) {
         maxElement = arr[i];
       }
     }
-    return maxElement;
   }
+  return maxElement;
 };
 
-var getRandomTransparency = function(r, g, b) {
+var getRandomTransparency = function (r, g, b) {
   var j = Math.random() + 0.1;
   var color = 'rgba(' + r + ', ' + g + ', ' + b + ', ' + j.toFixed(1) + ')';
   if (j >= 1) {
@@ -32,12 +32,12 @@ var getRandomTransparency = function(r, g, b) {
   return color;
 };
 
-var renderCloud = function(ctx, x, y, color) {
+var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-window.renderStatistics = function(ctx, NAMES, times) {
+window.renderStatistics = function (ctx, NAMES, times) {
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
@@ -49,19 +49,19 @@ window.renderStatistics = function(ctx, NAMES, times) {
 
   var maxTime = getMaxElement(times);
 
-  for(var i = 0; i < NAMES.length; i++) {
-    if (NAMES[i] == 'Вы') {
+  for (var i = 0; i < NAMES.length; i++) {
+    if (NAMES[i] === 'Вы') {
       ctx.fillStyle = '#000';
       ctx.fillText(NAMES[i], CLOUD_X + 2 * GAP + i * (BAR_WIDTH + BAR_GAP), CLOUD_HEIGHT - GAP);
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillRect(CLOUD_X + 2 * GAP + i * (BAR_WIDTH + BAR_GAP), CLOUD_HEIGHT - 2 * GAP -FONT_GAP, BAR_WIDTH, -1 * ((BAR_HEIGHT * times[i]) / maxTime));
+      ctx.fillRect(CLOUD_X + 2 * GAP + i * (BAR_WIDTH + BAR_GAP), CLOUD_HEIGHT - 2 * GAP - FONT_GAP, BAR_WIDTH, -1 * ((BAR_HEIGHT * times[i]) / maxTime));
       ctx.fillStyle = '#000';
       ctx.fillText(Math.round(times[i]), CLOUD_X + 2 * GAP + i * (BAR_WIDTH + BAR_GAP), CLOUD_HEIGHT - 4.5 * GAP + (-1 * ((BAR_HEIGHT * times[i]) / maxTime)));
     } else {
       ctx.fillStyle = '#000';
       ctx.fillText(NAMES[i], CLOUD_X + 2 * GAP + i * (BAR_WIDTH + BAR_GAP), CLOUD_HEIGHT - GAP);
       ctx.fillStyle = getRandomTransparency(0, 0, 139);
-      ctx.fillRect(CLOUD_X + 2 * GAP + i * (BAR_WIDTH + BAR_GAP), CLOUD_HEIGHT - 2 * GAP -FONT_GAP, BAR_WIDTH, -1 * ((BAR_HEIGHT * times[i]) / maxTime));
+      ctx.fillRect(CLOUD_X + 2 * GAP + i * (BAR_WIDTH + BAR_GAP), CLOUD_HEIGHT - 2 * GAP - FONT_GAP, BAR_WIDTH, -1 * ((BAR_HEIGHT * times[i]) / maxTime));
       ctx.fillStyle = '#000';
       ctx.fillText(Math.round(times[i]), CLOUD_X + 2 * GAP + i * (BAR_WIDTH + BAR_GAP), CLOUD_HEIGHT - 4.5 * GAP + (-1 * ((BAR_HEIGHT * times[i]) / maxTime)));
     }
